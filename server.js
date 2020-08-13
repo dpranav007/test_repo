ba19c223421cb59215ed37b1d6f33e7724572817
Pranav <dpranav007@gmail.com>
2020-08-13 23:48:43 +0530

    init

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const http = require('http')
-const port = 80
-
+const http = require("http");
+const port = 80;
+console.log(process.env.NODE_ENV);
 const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
+  console.log(request.url);
+  response.end("Hello Node.js Server!");
+};
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log("something bad happened", err);
   }
 
-  console.log(`server is listening on ${port}`)
-})
+  console.log(`server is listening on ${port}`);
+});
